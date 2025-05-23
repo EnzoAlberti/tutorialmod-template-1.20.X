e2292a1b7ccc0c4f780d3183290f758f8017441f
EnzoAlberti <enzogfalberti@gmail.com>
2025-05-23 14:49:01 -0300

    Added items

--- src/main/java/net/norguet/tutorialmod/Tutorialmod.java
+++ src/main/java/net/norguet/tutorialmod/Tutorialmod.java
@@ -2,16 +2,19 @@ package net.norguet.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.norguet.tutorialmod.item.ModItemGroups;
+import net.norguet.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Tutorialmod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	
+
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
